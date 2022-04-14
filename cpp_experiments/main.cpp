2e6aa7e89eee73d0db4e7b2f6d2bd0277f9f15ff
DilshodN <dilshod8471@gmail.com>
2022-04-14 20:49:18 +0700

    iwannabeer

--- cpp_experiments/main.cpp
+++ cpp_experiments/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <algorithm>
+#include <stack>
 
-template<int...args>
-int sum(){
-    return (0 + ... + args);
+bool valid_braces(const std::string& braces) {
+    std::string openBraces = "([{";
+    std::string closedBraces = ")]}";
+    std::stack<char> st;
+
+    for (const auto &elem: braces) {
+        if (std::find(openBraces.begin(), openBraces.end(), elem) != openBraces.end()) {
+            st.push(elem);
+        }
+        else{
+            auto idx = std::find(closedBraces.begin(), closedBraces.end(), elem) - closedBraces.begin();
+
+            if (not st.empty() and openBraces[idx] == st.top()) {
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return st.empty();
 }
 
-int main(){
-    std::cout << sum<1, 2, 3>() << std::endl;
+int main() {
+    std:: cout << valid_braces("]]]");
     return 0;
 }
